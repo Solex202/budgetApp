60a23b9a573a301efccc7fca0f8345840322f3c2
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-04 10:45:54 +0100

    my budget app

---
+++ src/NewApp.js
@@ -0,0 +1,14 @@
+import Budget from "./Budget"
+import "./NewApp.css"
+
+function NewApp(){
+
+    return(
+
+        <div className="app-container">
+            <Budget/>
+        </div>
+    );
+}
+
+export default NewApp;
