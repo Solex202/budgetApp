f93c2294c0682ccd9be3eed206e4c7d61ae27bc2
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-04 10:45:31 +0100

    my budget app

---
+++ src/Travel.js
@@ -0,0 +1,54 @@
+const Travel = () => {
+    return (  
+        <div></div>
+    //     const links = [
+    //         {
+    //           buttonName: 'Reviews',
+    //         },
+    //         { buttonName: 'Tips',
+    //         },
+    //         {  buttonName: 'Alerts',
+    //         },
+    //         {  buttonName: 'Blogs'
+    //         }
+    //       ]
+    //       return (
+    //         <div className="app-container">
+    //           <div className="header-container">
+    //             <div className="header"> 
+    //               <Logo/>
+    //               <div style={{display: 'flex', justifyContent: 'space-between', width: '20%', color: 'white'}}>
+    //                 {links.map((link) => <p>{link.buttonName}</p>)}
+    //               </div>
+    //               <AuthButton/>
+    //             </div>
+    //             <div className="header-text">
+    //               <h1>Discover a Beautiful <br/> Place With Us</h1>
+    //               <p>Would you explore nature paradise in the world ,let's find the <br/> best destination in the world with us</p>
+    //             </div>
+    //           </div>
+    //           <div className="toggle-search">
+    //             {/* <ToggleButton/> */}
+    //             <ToggleButton  image=""  btnText="Flight"/>
+    //             <ToggleButton  image=""  btnText="Hotel"/>
+    //           </div>
+    //           <div className="box">
+    //           <FloatBox/>
+    //           </div>
+    //           <div className="special-container">
+    //             <div className="container-text">
+    //               <p>SPECIAL FEATURE</p>
+    //               <h2>See Some Benefits Joining Us</h2>
+    //             </div>
+    //             <div>
+    //             <Features />
+    //             </div>
+                
+    //           </div>
+              
+              
+    // );
+    );
+}
+ 
+export default Travel;
