6c2087489fe9efff7f4856c00b79403ff5d41cb2
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-25 10:48:11 +0100

    my budget app

---
+++ src/components/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const HeroSection = () => {
+  return (
+    <div className="below-header">
+        <div className="main-text-holder">
+            <h1>Discover a beautiful <br/> place with us</h1>
+            <p>Would you enjoy nature paradise in the world, let's find the <br/>best destination in the world with us</p>
+        </div>
+                    
+    </div>
+  )
+}
+
+export default HeroSection;
